7926d4a1cc1b3687cd21cd94bd64e21547d5f833
Stefan Jovanovic <10219stefanjovanovic@etsntesla.edu.rs>
2022-10-11 10:47:16 +0200

    mornarcic

--- src/more/Mornar.java
+++ src/more/Mornar.java
@@ -1,11 +1,28 @@
 package more;
 
 public class Mornar {
-    public int kvalitet;
+    private int kvalitet;
+public String aa;
+    public void setKvalitet(int kvalitet) {
+
+        if(kvalitet<0){
+            this.kvalitet=0;
+        } else if (kvalitet>100) {
+            this.kvalitet=100;
+        }
+        else {
+            this.kvalitet = kvalitet;
+        }
+        }
+
+    public Mornar(String a, int k) {
+    kvalitet=k;
+        aa=a;
+    }
 
     @Override
     public String toString() {
-        return "";
+        return "M_"+aa+" : "+kvalitet;
     }
 }
 
