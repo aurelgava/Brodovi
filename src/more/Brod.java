151add472e1d9b651143859fcacf25c306fba201
ciziki <10219igorcizik@etsntesla.edu.rs>
2022-10-11 10:10:26 +0200

    Brod polja i konstruktor

--- src/more/Brod.java
+++ src/more/Brod.java
@@ -1,5 +1,21 @@
 package more;
+import java.lang.*;
 
 public class Brod {
+    int brojposade;
+    String naziv;
+    Mornar kapetan = new Mornar(Nikola, 90);
+
+    public Brod() {
+    }
+
+    public Brod(int brojposade, String naziv, Mornar kapetan) {
+        this.brojposade = brojposade;
+        this.naziv = naziv;
+        this.kapetan = kapetan;
+    }
+
+
+
 
 }
